e7efcc79b8a35558fef1999a33f8d576b4a2c3f3
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-08-08 21:01:26 -0700

    finalized pokemon api call, returned desired pokemon features and pushed to html elements

--- app.js
+++ app.js
@@ -21,13 +21,10 @@ $("#akey").on("click", function () {
 
 // onclick function for when the user clicks the down arrow
 
-
 $("#downkey").click(function () {
     $("html, body").animate({ scrollTop: $(document).height() }, 1600);
     return false;
 });
-
-
 
 // ====================== END OF INTRO SCREEN ========================
 
@@ -36,180 +33,21 @@ $("#downkey").click(function () {
 AOS.init({
     duration: 3000,
     delay: 1000,
-  });
+});
 
-  // ========= End of Method to call on the AOS library for card animation=======================
+// ========= End of Method to call on the AOS library for card animation=======================
 
 //this variable will hold the output of the faceAPI call 
 //this will be a list of key/value pairs related to emotions and % of emotion detected 
 var highestEmotion = "";
 
-//Poke Array Object 
-// var pokemonEmotions = {
-//     anger: [3, 14, 15, 18, 20, 24, 28, 29, 33, 34, 55, 56, 57, 62, 64, 65, 76, 83, 85, 100, 105, 106, 107, 115, 117, 123, 125, 127, 130, 145],
-//     contempt: [2, 5, 6, 7, 8, 9, 17, 38, 45, 53, 58, 59, 67, 73, 91, 96, 97, 99, 102, 126, 128, 131, 133, 134, 135, 146, 150],
-//     disgust: [12, 16, 68, 74, 78, 88, 89, 95, 108, 109, 110],
-//     happy: [1, 4, 25, 26, 35, 36, 39, 40, 43, 61, 66, 87, 103, 113, 122, 149],
-//     neutral: [10, 13, 46, 49, 50, 51, 71, 77, 79, 81, 82, 84, 98, 111, 116, 120, 121, 129, 132, 137, 138, 140, 141, 143, 147, 148],
-//     sad: [11, 30, 44, 60, 63, 104],
-//     surprise: [21, 23, 31, 37, 46, 54, 69, 70, 80, 86, 90, 112, 118, 136, 144]
-// }
-
-// console.log(pokemonEmotions[highestEmotion]);
-
-//This function is essentially what runs when the user hits their submit button to upload their image
-
-//Initialize function when user clicks the submit button
-$("#submitButton").on("click", function () {
-    //Defining count variable to iterate through ajax calls while avoiding use of for loop (due to asynchronous-ness)
-    var count = 1;
-
-    //IGNORE, OLD CODE
-    // //Create empty array to store ajax call results, end result should be an array of all pokemon called + details
-    // var pokemonAvailable = []
-
-    //Angry Poke
-
-
-    var angryPoke = [3, 14, 15, 18, 20, 24, 28, 29, 33, 34, 55, 56, 57, 62, 64, 65, 76, 83, 85, 100, 105, 106, 107, 
-    115, 117, 123, 125, 127, 130, 145];
-    
-    
-    randomAngry = angryPoke[Math.floor(Math.random() * angryPoke.length)];
-
-    console.log("TEST",randomAngry)
-    
-    var angryPokeDeets = [];
-
-    //Contempt Poke
-    var contemptPoke = [2, 5, 6, 7, 8, 9, 17, 38, 45, 53, 58, 59, 67, 73, 91, 96, 97, 99, 102, 126, 128, 131, 133, 134, 135, 146, 150]
-    var contemptPokeDeets = []
-
-    //Disgust Poke
-    var disgustPoke = [12, 16, 68, 74, 78, 88, 89, 95, 108, 109, 110]
-    var disgustPokeDeets = []
-
-    //Fear Poke
-    var fearPoke = [19, 22, 27, 32, 41, 42, 48, 52, 71, 75, 92, 93, 94, 101, 114, 119, 124, 139, 142]
-    var fearPokeDeets = []
-
-    //Happiness Poke
-    var happyPoke = [1, 4, 25, 26, 35, 36, 39, 40, 43, 61, 66, 87, 103, 113, 122, 149]
-    var happyPokeDeets = []
-
-    //Neutral Poke
-    var neutralPoke = [10, 13, 46, 49, 50, 51, 71, 77, 79, 81, 82, 84, 98, 111, 116, 120, 121, 129, 132, 137, 138, 140, 141, 143, 147, 148]
-    var neutralPokeDeets = []
-
-    //Sad Poke
-    var sadPoke = [11, 30, 44, 60, 63, 104]
-    var sadPokeDeets = []
-
-    //Surprise Poke
-    var surprisePoke = [21, 23, 31, 37, 46, 54, 69, 70, 80, 86, 90, 112, 118, 136, 144]
-    var surprisePokeDeets = []
-
-    //STEPS TO FETCH POKE
-    //1. Run Poke API to loop thru each poke
-    //2. For each poke, look at the stats + type of poke
-    //3. Based on stats + type of poke (plus whatever other arbitrary stats), push poke into one of 8 poke arrays
-    //4. After poke array is fully dealt with, then we run face api
-    //a. take user image, and get data from that when 'upload' button is hit. Determine which emotion class the face falls into
-    //5. When 'Submit' button is hit, link face emotion type w/ pokemon array of same emotion, return random poke from that array + stats
-
-
-    //Function w/ ajax call to pull data on however many pokemon
-    function fetchPokemon() {
-
-        // Here we construct our URL, and pick our pokemon by its 'id number' (pokedex number). In this case, we limit our results to the first 150 pokemon (OG) -- 50 for testing purposes
-        var queryURL1 = "https://pokeapi.co/api/v2/pokemon/" + count + "/"
-
-
-        // Write code to hit the queryURL with $ajax, 'GET' our returned results, and then take the response data
-        // and console log the specific elements we are returning
-        $.ajax({
-            url: queryURL1,
-            method: 'GET'
-        })
-            // .then() => is a method from the Promise library that promises to execute a callback function. The callback => is going to capture the data that comes back from our AJAX call
-            .then(function (response1) {
-
-                // console.log("response1", response1)
-                // console.log("POKEMON ID NUMBER", response1.id)
-
-                if (angryPoke.includes(response1.id)) {
-                    angryPokeDeets.push(response1)
-                }
-                else if (contemptPoke.includes(response1.id)) {
-                    contemptPokeDeets.push(response1)
-                }
-                else if (disgustPoke.includes(response1.id)) {
-                    disgustPokeDeets.push(response1)
-                }
-                else if (fearPoke.includes(response1.id)) {
-                    fearPokeDeets.push(response1)
-                }
-                else if (happyPoke.includes(response1.id)) {
-                    happyPokeDeets.push(response1)
-                }
-                else if (neutralPoke.includes(response1.id)) {
-                    neutralPokeDeets.push(response1)
-                }
-                else if (sadPoke.includes(response1.id)) {
-                    sadPokeDeets.push(response1)
-                }
-                else if (surprisePoke.includes(response1.id)) {
-                    surprisePokeDeets.push(response1)
-                }
-
-                // console.log("ARRAY OF ANGRY POKES", angryPokeDeets);
-                // console.log("ARRAY OF CONTEMPT POKES", contemptPokeDeets);
-                // console.log("ARRAY OF DISGUST POKES", disgustPokeDeets);
-                // console.log("ARRAY OF FEAR POKES", fearPokeDeets);
-                // console.log("ARRAY OF HAPPPY POKES", happyPokeDeets);
-                // console.log("ARRAY OF NEUTRAL POKES", neutralPokeDeets);
-                // console.log("ARRAY OF SAD POKES", sadPokeDeets);
-                // console.log("ARRAY OF SURPRISE POKES", surprisePokeDeets);
-
-                // OLD CODE ////////////////////////////////////
-                // //For each ajax call run, push the resulting details to our array outside of ajax
-
-                // pokemonAvailable.push(response1)
-
-                // //Testing console logs + information we want to use in face comparison
-                // // console.log(response1.name)
-                // // console.log(response1.stats)
-                // // console.log(response1.height)
-                // // console.log(response1.weight)
-                // // console.log(response1.sprites.front_default)
-                // console.log(pokemonAvailable)
-                //OLD CODE END ////////////////////////////////////
-
-                //Loop through number of pokemon that we want to have in our sample (bypasses for loop so that each call is fully executed before next one is run)
-                if (count < 50) {
-                    count = count + 1;
-                    // console.log('GRAND TOTAL:', count);
-                    fetchPokemon();
-                }
-            })
-
-        //CODE to GRAB RANDOM POKE FROM SELECTED ARRAY AND PUSH TO IMAGE
-        // var randomPoke = Math.floor(Math.random()*50)
-        // var randomPokeName = pokemonAvailable[randomPoke].name
-        // var randomPokeImg = pokemonAvailable[randomPoke].sprites.front_default
-        // console.log("Random Poke Name", randomPokeName )
-        // console.log("Random Poke Image", randomPokeImg)
-        // // return(randomPokeName, randomPokeImg)
-
-        // $("#pokeImage").html("<img src='"+randomPokeImg+"'>")
-    }
-
-    fetchPokemon();
-});
-
-
-
-
+//Define pokemon features that we want to return after call (globally)
+var pokemonName;
+var pokemonImage;
+var pokemonType;
+var pokemonAbility;
+var pokemonMoveOne;
+var pokemonMoveTwo;
 
 /*=============================================
 =User image upload (POST) and faceAPI call (POST)=
@@ -269,6 +107,11 @@ $(document).ready(function () {
                 })
                     //will caputue the promise returned by the POST
                     .then(function (res2) {
+
+                        //Empty placeholder image div
+                        $("#userImage").empty();
+                        //Put user's uploaded image into div
+                        $("#userImage").attr("src", uploadedImgLink)
                         //variable that stores the faceAttributes object spit out from the FaceAPI POST call
                         //example: "faceAttributes": { "emotion": { "anger": 0.0, "contempt": 0.0, "disgust": 0.0, "fear": 0.015, "happiness": 0.219, "neutral": 0.0, "sadness": 0.0, "surprise": 0.766 } }
                         let faceEmotions = res2.data[0].faceAttributes.emotion;
@@ -292,9 +135,9 @@ $(document).ready(function () {
                             anger: [3, 14, 15, 18, 20, 24, 28, 29, 33, 34, 55, 56, 57, 62, 64, 65, 76, 83, 85, 100, 105, 106, 107, 115, 117, 123, 125, 127, 130, 145],
                             contempt: [2, 5, 6, 7, 8, 9, 17, 38, 45, 53, 58, 59, 67, 73, 91, 96, 97, 99, 102, 126, 128, 131, 133, 134, 135, 146, 150],
                             disgust: [12, 16, 68, 74, 78, 88, 89, 95, 108, 109, 110],
-                            happy: [1, 4, 25, 26, 35, 36, 39, 40, 43, 61, 66, 87, 103, 113, 122, 149],
+                            happiness: [1, 4, 25, 26, 35, 36, 39, 40, 43, 61, 66, 87, 103, 113, 122, 149],
                             neutral: [10, 13, 46, 49, 50, 51, 71, 77, 79, 81, 82, 84, 98, 111, 116, 120, 121, 129, 132, 137, 138, 140, 141, 143, 147, 148],
-                            sad: [11, 30, 44, 60, 63, 104],
+                            sadness: [11, 30, 44, 60, 63, 104],
                             surprise: [21, 23, 31, 37, 46, 54, 69, 70, 80, 86, 90, 112, 118, 136, 144]
                         }
 
@@ -307,7 +150,6 @@ $(document).ready(function () {
                         //should be a random number from the array 
                         var choosenPokemon = choosenArray[randomNumber];
 
-                        
                         var queryURL1 = "https://pokeapi.co/api/v2/pokemon/" + choosenPokemon + "/"
                         //async call to the pokemonAPI
                         //should return just one pokemon 
@@ -316,9 +158,30 @@ $(document).ready(function () {
                             url: queryURL1,
                             method: 'GET'
                         }).then(function (res3) {
-                            console.log(res3.name);
+                            //Return pokemon name
 
-                        }).catch(function (err3) {
+                            pokemonName = res3.name
+                            console.log(pokemonName);
+                            //return pokemon image
+
+                            pokemonImage = res3.sprites.front_default
+                            console.log(pokemonImage);
+                            //return random pokemon ability
+
+                            pokemonAbility = res3.abilities[Math.floor(Math.random() * res3.abilities.length)].ability.name
+                            console.log(pokemonAbility);
+                            //Return pokemon type (only one, if two types)
+
+                            pokemonType = res3.types[Math.floor(Math.random() * res3.types.length)].type.name
+                            console.log(pokemonType);
+                            //return two random pokemon moves
+
+                            pokemonMoveOne = res3.moves[Math.floor(Math.random() * res3.moves.length)].move.name
+                            console.log(pokemonMoveOne);
+
+                            pokemonMoveTwo = res3.moves[Math.floor(Math.random() * res3.moves.length)].move.name
+                            console.log(pokemonMoveTwo);
+                        }).catch(function (err3) { //Error catching ////////////////////////////////////
                             console.error(err3);
                         })
                     })
@@ -329,8 +192,18 @@ $(document).ready(function () {
             })
             .catch(function (err) {
                 console.error(err);
-            });
+            }); // End of error catching ////////////////////////////////////////////////
 
     });
 });
 
+$("#submitButton").on("click", function () {
+
+    $("#pokeName").text(pokemonName)
+    $("#pokeImageReal").attr("src", pokemonImage)
+    $("#pokemonType").text(pokemonType)
+    $("#pokemonAbility").text(pokemonAbility)
+    $("#pokemonMoveTwo").text(pokemonMoveTwo)
+    $("#pokemonMoveOne").text(pokemonMoveOne)
+
+});
